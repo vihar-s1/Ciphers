dae54012d6a6d2f2936cd7bae2846849208857f8
Vihar <vihar2121@gmail.com>
2023-02-14 19:25:28 +0530

    AffineCipher Documentation and Docstrings

--- AffineCipher.py
+++ AffineCipher.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
-from os import system
 
 '''
 - Performs Encryption Decryption Operation for the AffineCipher
+- Encryption of Affine Cipher: C = a * P + b (mod 26)
+- Decryption of Affine Cipher: P = a^-1 * (C - b) (mod 26)
+- Here, P and C are plain text and cipher text resp.
+- a, b belong to Z26 ring. Also a must be an invertible element for cipher to work.
 '''
+
+from os import system
+import re
 
 __char_index = {chr(i+97): i for i in range(26)}
 __index_char = {i: chr(i+97) for i in range(26)}
@@ -11,16 +17,22 @@ __inverse = {1: 1, 3: 9, 5: 21, 7: 15, 9: 3, 11: 19, 13: 13, 15: 7, 17: 23,
              19: 11, 21: 5, 23: 17, 25: 25}
 
 
-def encrypt(text: str, key_a: int, key_b: int) -> str | None:
-    '''
-    - Encrypts the given plain text using key (key_a, key_b) if and only if key_a is invertible
-    - Returns cipher text if successfully encrypted else returns None
-    '''
+def encrypt(text: str, key_a: int, key_b: int) -> str:
+    """Performs encryption on the text using key (a, b)
+
+    Args:
+        text (str): the plain text string to be encrypted
+        key_a (int): multiplicative part of the key
+        key_b (int): additive part of the key
+
+    Returns:
+        str: returns cipher text on successfull encryption else returns None
+    """
     # inverse of key_a is not found so we return None
     if __inverse.get(key_a, None) is None:
         return None
 
-    text = list(text)
+    text = list( str(re.sub(r'[^a-zA-Z]+', "", text)).lower() )
     cipher = []
 
     for char in text:
@@ -29,17 +41,24 @@ def encrypt(text: str, key_a: int, key_b: int) -> str | None:
     return ''.join(cipher)
 
 
-def decrypt(cipher: str, key_a: int, key_b: int) -> str | None:
-    '''
-    - Decrypts the given cipher text using key (key_a, key_b) if and only if key_a is invertible
-    - Returns the plain text if successfully decryted else returns None
-    '''
+def decrypt(cipher: str, key_a: int, key_b: int) -> str:
+    """Performs affine cipher decryption on cipher text using key (a, b)
+
+    Args:
+        cipher (str): cipher text to be decrypted
+        key_a (int): multiplicative part of the key
+        key_b (int): additive part of the key
+
+    Returns:
+        str: returns the plain text on successful decryption else returns None
+    """
     if __inverse.get(key_a, None) is None:
         print("AffineCipher: decrypt(): gcd(a, 26) = 1 condition not satisfied for the given key tuple")
         return None
 
     text = []
-    cipher = list(cipher)
+    cipher = list( str(re.sub(r'[^a-zA-Z]+', "", cipher)).lower() )
+
 
     # we perform + 26 while subtracting key_b to ensure that modulo operation is performed 
     # on a positive value only as adding 26 is same as adding 0 in modulo-26 addition
@@ -76,8 +95,8 @@ def __main__():
 
 
 if __name__ == "__main__":
-    __main__()
-    # text = input().replace(' ', '')
-    # cipher = encrypt(text, 7, 5)
-    # print(cipher)
-    # print(decrypt(cipher, 7, 5))
+    # __main__()
+    text = input()
+    cipher = encrypt(text, 5, 10)
+    print(cipher)
+    print(decrypt(cipher, 5, 10))
--- README.md
+++ README.md
@@ -3,13 +3,29 @@
 Consists of implementations of various classical and private key cryptographic algorithms.
 
 ## Classical Cryptography Ciphers
+<hr>
 
 ### Shift Cipher
 The most basic form of cipher. It involves shifting operation of the plain text to generate cipher text. Based on the key selected from 0 to 25, the alphabet is shifted by that much to obtain the encryted text.
 
-> plain text: "Hello World"<br>
-key = 5 <br>
-Cipher generated: "mjqqt btwqi"
+> - encrypt( text="Hello World", key=5 ) <br>
+> - The function returns following string: "mjqqt btwqi" (without space)
 
+> - decrypt( text="mjqqt btwqi", key=5 ) <br>
+> - The function returns following string: "hello world" (without space)
+
+
+### Affine Cipher
+- Affine cipher uses a pair of numbers (a, b) as key where $a, b \in \Z_{26}$.
+- The encryption function is defined as $ y = a*x + b (mod 26) $ where $x$ is plain text and $y$ is the cipher text.
+- Similarly decryption function performs following operation $ x = a^{-1} * (y - b) (mod 26) $.
+- Note that as we use $a^{-1}$ here and $a\in\Z_{26}$ which a ring. So $a$ must be an invertible element of $\Z_{26}$. Otherwise, decryption is not possible.
+
+> - encrypt( text="Hello World", key_a=5, key_b=10 ) <br>
+> - The function returns following string: "tennc qcrnz" (without space)
+
+> - decrypt( text="tennc qcrnz", key_a=5, key_b=10 ) <br>
+> - The function returns following string: "hello world" (without space)
 
 ## Private Key Cryptography
+<hr>
